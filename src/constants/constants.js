34ef47b1cce7788ab3eff4793700b385a4549813
Aleksandr Smelov <smelovalex14@gmail.com>
2024-07-03 13:51:20 +0300

    host update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://api.alex.movie-explorer.nomoredomainsmonster.ru';
+export const BASE_URL = 'http://localhost:3000';
 // export const BASE_URL = 'http://localhost:3000';
